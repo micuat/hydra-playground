4b3989e3aa8882d90bf7c7205bf7ac8b8f20a255
micuat <micuat@gmail.com>
2025-12-05 19:10:57 +0200

    change to hydra clipboard template

--- app/stores/machine.js
+++ app/stores/machine.js
@@ -1,100 +1,37 @@
 import TextTweenElement from "../components/text-tween-element.js";
 
 export default function(state, emitter) {
-  state.prompt = "hola"
-  state.centroid = {x: 0, y: 0};
   state.showModal = true;
-  
-  state.assets = [
-    {
-      color: "/0_IMG_5996.JPG",
-      edge: "/e0_IMG_5996.JPG",
-      name: "pool tension",
-    },
-    {
-      color: "/1_IMG_6022.JPG",
-      edge: "/e1_IMG_6022.JPG",
-      name: "quadruple tension",
-    },
-    {
-      color: "/2_IMG_5991.JPG",
-      edge: "/e2_IMG_5991.JPG",
-      name: "disturbed tension",
-    },
-    {
-      color: "/3_IMG_6010.JPG",
-      edge: "/e3_IMG_6010.JPG",
-      name: "grounded tension",
-    },
-    {
-      color: "/5_IMG_6019.JPG",
-      edge: "/e5_IMG_6019.JPG",
-      name: "classic tension",
-    },
-    {
-      color: "/4_IMG_6013.JPG",
-      edge: "/e4_IMG_6013.JPG",
-      name: "kinky tension",
-    },
-    {
-      color: "/6_IMG_6005.JPG",
-      edge: "/e6_IMG_6005.JPG",
-      name: "clean double tension",
-    },
-    {
-      color: "/7_IMG_6012.JPG",
-      edge: "/e7_IMG_6012.JPG",
-      name: "rubberband tension",
-    },
-    {
-      color: "/8_IMG_6004.JPG",
-      edge: "/e8_IMG_6004.JPG",
-      name: "intension",
-    },
-  ]
-
-  let curIndex = 0;
-  state.currentAsset = state.assets[curIndex];
-
-  emitter.on("next asset", () => {
-    curIndex = (curIndex + 1) % state.assets.length;
-    state.currentAsset = state.assets[curIndex];
-    state.cache(TextTweenElement, 'my-text').textTween(state.currentAsset.name);
-    emitter.emit("render");
-    renderHydra();
-  });
-
-  emitter.on("set asset", (i) => {
-    curIndex = i % state.assets.length;
-    state.currentAsset = state.assets[curIndex];
-    state.cache(TextTweenElement, 'my-text').textTween(state.currentAsset.name);
-    emitter.emit("render");
-    renderHydra();
-  });
 
   function renderHydra() {
-    s0.initImage(state.currentAsset.color);
-    s1.initImage(state.currentAsset.edge);
-
-    src(o1).blend(s0,0.03).out(o1)
-    src(o2).blend(s1,0.03).out(o2)
-
-    src(o1).layer(src(o2).saturate(0)).layer(
-      src(s1).invert().luma(0.4).invert()
-      .mask(shape(4,0,0.7)
-      //.mult(osc(10,-0.1).thresh(.5,.4).kaleid(999)
-        .modulate(solid(()=>-state.centroid.x, ()=>-state.centroid.y), 1))
-      //)
-      .modulate(
-        osc(6,0,1.6).modulate(noise(.1,0.01).sub(gradient()),1)
-        .mult(osc(10,-0.1).thresh(.5,.4).kaleid(999).modulate(solid(()=>-state.centroid.x, ()=>-state.centroid.y), 1)),0.1
-      )
-    )
-    .scale(1, ()=>window.innerHeight/window.innerWidth)
-    .out()
+    src(o0).modulate(gradient().pixelate(2,2).brightness(-.5), -0.1)
+    .layer(s0).out();
   }
 
   emitter.on("DOMContentLoaded", () => {
     renderHydra();
+
+    document.onpaste = function (event) {
+      var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+      console.log(JSON.stringify(items)); // might give you mime types
+      for (var index in items) {
+          var item = items[index];
+          if (item.kind === 'file') {
+              var blob = item.getAsFile();
+              var reader = new FileReader();
+              reader.onload = function (event) {
+                  console.log(event.target.result); // data url!
+
+                  let img = new Image();
+                  img.src = event.target.result;
+                  img.onload = function(e) {
+                    console.log(e)
+                    s0.init({ src: img });
+                  };
+              }; 
+              reader.readAsDataURL(blob);
+          }
+      }
+    };
   });
 }
--- app/views/main.js
+++ app/views/main.js
@@ -2,8 +2,27 @@
 import html from "choo/html";
 
 import HydraCanvas from "../components/hydra-canvas.js";
-import DrawerElement from "../components/drawer.js";
-import TextTweenElement from "../components/text-tween-element.js";
+
+async function pasteImage(e) {
+  try {
+    const clipboardContents = await navigator.clipboard.read();
+    for (const item of clipboardContents) {
+      if (!item.types.includes("image/png") && !item.types.includes("image/jpg")) {
+        throw new Error("Clipboard does not contain PNG image data.");
+      }
+      const type = item.types.includes("image/png") ? "image/png" : "image/jpg";
+      const blob = await item.getType(type);
+      let img = new Image();
+      img.src = URL.createObjectURL(blob);
+      img.onload = function(e) {
+        console.log(e)
+        s0.init({ src: img });
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 // export module
 export default function(state, emit) {
@@ -13,43 +32,16 @@ export default function(state, emit) {
       <div class="absolute left-0 top-0 z-[-2] w-screen h-screen">
         ${ state.cache(HydraCanvas, 'my-hydra').render(state, emit) }
       </div>
-      <div class="absolute left-0 top-0 z-[-1] w-screen h-screen">
-        ${ state.cache(DrawerElement, 'my-draw').render(state, emit) }
-      </div>
       <button class="absolute left-2 top-2 text-xl" onclick=${ question }>ℹ️</button>
-      <div class="w-full flex flex-col items-center text-center justify-end h-screen">
-        <div class="text-4xl font-bold bg-white/80 mb-24 uppercase">
-          ${ state.cache(TextTweenElement, 'my-text').render(state, emit) }
-        </div>
-      </div>
       <div class="absolute left-0 top-0 z-10 border-2 border-black bg-white m-2 p-2 ${ state.showModal ? "" : "hidden" }">
         <button class="text-xl" onclick=${ hideModal }>❎</button>
-        <div class="font-bold">Rubber Band Tensions</div>
-        <div>Drag the blobby body to explore tensions :)</div>
-        <div class="font-bold">Content by:</div>
-        <div><a href="https://annabrynskov.com/" target="_blank">Anna Brynskov</a></div>
-        <div><a href="https://joanachicau.com/" target="_blank">Joana Chicau</a></div>
-        <div>Sophie Grimme</div>
-        <div><a href="https://naotohieda.com" target="_blank">Naoto Hieda</a></div>
-        <div class="font-bold">Web version by:</div>
-        <div><a href="https://naotohieda.com" target="_blank">Naoto Hieda</a></div>
-        <div class="font-bold">Made at:</div>
-        <div><a href="https://bodypoliticschi.wordpress.com/" target="_blank">Body Politics Workshop at CHI 2025</a></div>
+        <div class="font-bold">ctrl+v or press below to paste image</div>
+        <input class="border-2 border-black" type="text">
+        <button class="border-2 border-black" onclick=${ pasteImage }>paste</button>
       </div>
     </div>
   `;
-//   <div class="relative w-[400px] z-0">
-//   ${ state.cache(HydraCanvas, 'my-hydra').render(state, emit) }
-// </div>
-// <div class="relative w-[400px] z-0">
-//   ${ state.cache(DrawerElement, 'my-draw').render(state, emit) }
-// </div>
 
-{/* <button class="border-2 border-black" onclick=${ next }>next</button> */}
-
-  function next() {
-    emit("next asset");
-  }
   function question(e) {
     e.preventDefault();
     state.showModal = !state.showModal;
